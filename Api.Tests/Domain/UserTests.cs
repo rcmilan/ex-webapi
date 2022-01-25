40073edae5ae9cb1ed4efc85e975461227ec18e4
Ricardo Milan <ricardo@intercambiodireto.com>
2022-01-25 08:51:03 -0300

    testes para user

---
+++ Api.Tests/Domain/UserTests.cs
@@ -0,0 +1,25 @@
+﻿using Api.Domain.Models;
+using NUnit.Framework;
+using System;
+
+namespace Api.Tests.Domain
+{
+    internal class UserTests
+    {
+        [Test]
+        [TestCase("username", "email@email.com")]
+        public void ShouldBuild(string name, string email)
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+
+            // Act
+            var result = new User(id, name, email);
+
+            // Assert
+            Assert.AreEqual(id, result.ID);
+            Assert.AreEqual(name, result.Name);
+            Assert.AreEqual(email, result.Email);
+        }
+    }
+}
