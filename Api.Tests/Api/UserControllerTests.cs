568f6f4b0dca4f7d3af3acb6c4ca53d3cb1812ac
Ricardo Milan <ricardo@intercambiodireto.com>
2022-01-25 09:24:04 -0300

    adicionando web app factory

---
+++ Api.Tests/Api/UserControllerTests.cs
@@ -0,0 +1,31 @@
+﻿using Api.Domain.Models;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Api.Tests.Api
+{
+    internal class UserControllerTests
+    {
+        [Test]
+        public async Task ShouldAddUser()
+        {
+            // Arrange
+            await using var application = new WebAppFactory("Development");
+            var client = application.CreateClient();
+
+            var user = new User(System.Guid.NewGuid(), "username", "user@email.com");
+            var byteContent = WebAppFactory.BuildContent(user);
+
+            // Act
+            var response = await client.PostAsync("/api/user", byteContent);
+
+            // Assert
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+    }
+}
---
+++ Api.Tests/Api/WebAppFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace Api.Tests.Api
+{
+    internal class WebAppFactory : WebApplicationFactory<Program>
+    {
+        public readonly string _environment;
+        public WebAppFactory(string environment = "Development")
+        {
+            _environment = environment;
+        }
+
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.UseEnvironment(_environment);
+
+            return base.CreateHost(builder);
+        }
+
+        public static ByteArrayContent BuildContent<T>(T entity)
+        {
+            var myContent = JsonConvert.SerializeObject(entity);
+            var buffer = System.Text.Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return byteContent;
+        }
+    }
+}
