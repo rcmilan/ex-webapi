de4e7b19da4dc5322f30ea8f930cbb9246913aa8
Ricardo Milan <ricardo@intercambiodireto.com>
2022-01-26 08:28:37 -0300

    adicionando helper para testes

---
+++ Api.Tests/Helpers.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Api.Tests
+{
+    internal class Helpers
+    {
+        public static ByteArrayContent BuildContent<T>(T entity)
+        {
+            var myContent = JsonConvert.SerializeObject(entity);
+            var buffer = Encoding.UTF8.GetBytes(myContent);
+            var byteContent = new ByteArrayContent(buffer);
+            byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            return byteContent;
+        }
+    }
+}
